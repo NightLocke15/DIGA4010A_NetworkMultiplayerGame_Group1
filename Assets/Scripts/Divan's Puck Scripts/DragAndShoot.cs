659a111df5b0560d6a61f000afd8801465257efb
Kiewiet246 <kiewiet.lombaard01@gmail.com>
2025-04-17 16:44:52 +0200

    Kind of added controller support

--- Assets/Scripts/Divan's Puck Scripts/DragAndShoot.cs
+++ Assets/Scripts/Divan's Puck Scripts/DragAndShoot.cs
@@ -18,14 +18,20 @@ public class DragAndShoot : MonoBehaviour
     [SerializeField] private Rigidbody rb; //Rigidbody of the puck
 
     [Header("Gamepad Settings: Mouse")] //Variables used to create a virtual mouse that the gamepad can use
-    private Mouse virtualMouse;
     [SerializeField] private RectTransform cursorTransform;
+    private Mouse virtualMouse;
+    private Mouse realMouse;
+    private Mouse currentMouse;
     [SerializeField] private RectTransform canvasRectTransform;
     [SerializeField] private Canvas canvas;
     [SerializeField] private Camera PlayerCamera;
-    [SerializeField] private float cursorSpeed = 1000f; 
-    
+    [SerializeField] private float cursorSpeed = 1000f;
+    [SerializeField] private float padding = 35f;
     private bool previousMouseState;
+
+    private string previousControlScheme = "";
+    private const string gamepadScheme = "Gamepad";
+    private const string mouseScheme = "Keyboard&Mouse";
     
     [Header("Controller")]
     private CharacterController controller;
@@ -41,19 +47,27 @@ public class DragAndShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (previousControlScheme != playerInput.currentControlScheme)
+        {
+          // Debug.Log("Hello");
+            OnControlsChange(playerInput);
+        }
+        previousControlScheme = playerInput.currentControlScheme;
     }
 
     private void OnEnable()
     {
+        realMouse = Mouse.current;
         if (virtualMouse == null)
         {
             virtualMouse = (Mouse) InputSystem.AddDevice("VirtualMouse");
+          //  Debug.Log(virtualMouse.name + " If");
         }
         
         else if (!virtualMouse.added)
         {
-            InputSystem.AddDevice("VirtualMouse");
+           InputSystem.AddDevice(virtualMouse);
+           Debug.Log(virtualMouse.name + " else");
         }
         InputUser.PerformPairingWithDevice(virtualMouse, playerInput.user);
 
@@ -64,11 +78,14 @@ public class DragAndShoot : MonoBehaviour
         }
         
         InputSystem.onAfterUpdate += UpdateMotion;
+        playerInput.onControlsChanged += OnControlsChange;
     }
 
     private void OnDisable()
     {
+        if (virtualMouse != null && virtualMouse.added)InputSystem.RemoveDevice(virtualMouse);
         InputSystem.onAfterUpdate -= UpdateMotion;
+        playerInput.onControlsChanged -= OnControlsChange;
     }
 
     private void UpdateMotion()
@@ -85,8 +102,8 @@ public class DragAndShoot : MonoBehaviour
         Vector2 currentPos = virtualMouse.position.ReadValue();
         Vector2 newPos = currentPos + deltaValue;
         
-        newPos.x = Mathf.Clamp(newPos.x, 0, Screen.width);
-        newPos.y = Mathf.Clamp(newPos.y, 0, Screen.height);
+        newPos.x = Mathf.Clamp(newPos.x, padding, Screen.width - padding);
+        newPos.y = Mathf.Clamp(newPos.y, padding, Screen.height- padding);
         
         InputState.Change(virtualMouse.position, newPos);
         InputState.Change(virtualMouse.delta, deltaValue);
@@ -94,6 +111,7 @@ public class DragAndShoot : MonoBehaviour
         bool rightTriggerIsPressed = Gamepad.current.rightShoulder.IsPressed();
         if (previousMouseState != rightTriggerIsPressed)
         {
+          //  Debug.Log("Clicked");
             virtualMouse.CopyState<MouseState>(out var mouseState);
             mouseState.WithButton(MouseButton.Left, rightTriggerIsPressed);
             InputState.Change(virtualMouse, mouseState);
@@ -107,19 +125,45 @@ public class DragAndShoot : MonoBehaviour
     {
         Vector2 anchorPos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, pos, canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : PlayerCamera, out anchorPos);
+        cursorTransform.anchoredPosition = anchorPos;
     }
 
-    public void OnClick(InputValue value)
+    private void OnControlsChange(PlayerInput playerInput)
+    {
+    // Debug.Log("Change");
+        if (playerInput.currentControlScheme == mouseScheme && previousControlScheme != mouseScheme)
+        {
+          //  Debug.Log("K&M");
+            cursorTransform.gameObject.SetActive(false);
+            Cursor.visible = true;
+            realMouse.WarpCursorPosition(virtualMouse.position.ReadValue());
+            previousControlScheme = mouseScheme;
+            currentMouse = realMouse;
+        }
+        
+        else if (playerInput.currentControlScheme == gamepadScheme && previousControlScheme != gamepadScheme)
+        {
+            //Debug.Log("Gamepad");
+            cursorTransform.gameObject.SetActive(true);
+            Cursor.visible = false;
+            InputState.Change(virtualMouse.position, realMouse.position.ReadValue());
+            AnchorCursor(realMouse.position.ReadValue());
+            previousControlScheme = gamepadScheme;
+            currentMouse = virtualMouse;
+        }
+    }
+
+    public void OnAttack(InputValue value)
     {
         if (value.isPressed) //Checks if Click-action is pressed
         {
-           Debug.Log("Click");
-            Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out hit); //Raycast to select objects we want to interact with
+          // Debug.Log("Pressed");
+            Physics.Raycast(PlayerCamera.ScreenPointToRay(currentMouse.position.ReadValue()), out hit); //Raycast to select objects we want to interact with
             if (hit.collider != null) // Checks if we hit something
             {
                 if (hit.collider.tag == "Puck") //Checks if we hit a puck
                 {
-                    StartPos = Mouse.current.position.ReadValue(); //Gets the current mouse position of when the button is pressed down
+                    StartPos = currentMouse.position.ReadValue(); //Gets the current mouse position of when the button is pressed down
                     rb = hit.collider.gameObject.GetComponent<Rigidbody>(); // Get the rigidbody of the puck
                 }
             }
@@ -129,12 +173,13 @@ public class DragAndShoot : MonoBehaviour
 
         else //Is called when the button is released
         {
+         //  Debug.Log("Released");
             if (hit.collider != null) // Checks if we hit something
             {
                 if (hit.collider.tag == "Puck") //Checks if we hit a puck
                 {
                     Vector3 direction = new Vector3(); 
-                    EndPos = Mouse.current.position.ReadValue(); //Grab the position of the mouse when the button is released
+                    EndPos = currentMouse.position.ReadValue(); //Grab the position of the mouse when the button is released
                     direction = new Vector3(StartPos.x - EndPos.x, 0f, StartPos.y - EndPos.y).normalized; //We get only the direction between the start and end pos
                     float mag = new Vector3(StartPos.x - EndPos.x, 0f, StartPos.y - EndPos.y).magnitude; //We get the lenght between start and end pos
                     float clampedMag = Mathf.Clamp(mag, 0, MaxLength); //We put a max limit on the lenght
